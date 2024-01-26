3e1b083029d19f8664617e10f1b4df36730b589b
Mbuelo Mandy Khorommbi <mmkhorommbi@gmail.com>
2024-01-26 12:28:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Mandy mandy = new Mandy();
+    mandy.setHair();
+    mandy.Grey();
+    Kondi kondi = new Kondi();
+    kondi.setHair();
+    kondi.Grey();
+    }
+
+}
